a0b056092ac65fb24bd97e2b41ea3330d3de231a
Jim Aloo Juma <50961939+Jimaloo@users.noreply.github.com>
2020-10-05 13:04:43 +0300

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -35,19 +35,14 @@ public class InfoActivity extends AppCompatActivity {
         }
 
 
+
     }
-
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
 
     public void createPhoneIntent(View view){
+
+        Uri phoneUri = Uri.parse("tel:0232093230923");
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-        phoneIntent.setData(Uri.parse("tel: 0757573492"));
+        phoneIntent.setData(phoneUri);
         startActivity(phoneIntent);
     }
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
 }
